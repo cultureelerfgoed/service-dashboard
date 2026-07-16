e48410dd5fa20d6e3f99b6da432de1018f928bb8
Blok <j.s.blok@cultureelerfgoed.nl>
2026-07-16 14:22:39 +0200

    adding poolparty tests with auth

--- get_service_status.py
+++ get_service_status.py
@@ -1,15 +1,34 @@
+import os
 import json
 from urllib.parse import urlsplit
 import requests
 from datetime import date, timedelta, datetime, timezone
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-
+TOKEN = os.getenv('POOLPARTY_TOKEN')
 STATES = {
     'OK': '![#339900](https://placehold.co/5x5/339900/339900.png)',
     'WARNING': '![#ec942c](https://placehold.co/5x5/ec942c/ec942c.png)',
     'FAIL': '![#f03c15](https://placehold.co/5x5/f03c15/f03c15.png)',
 }
+
+def poolparty_test(url: str, description: str, token: str) -> str:
+    try:
+        if token:
+            response = requests.get(url, allow_redirects=True, headers={'Authorization': f'{token}'}, timeout=120)
+            if response.status_code == 200:
+                status = STATES['OK']
+            else:
+                status = STATES['FAIL']
+            millis = response.elapsed / timedelta(milliseconds=1)
+    
+            return format_status(status, f'[{urlsplit(url).netloc}]({url}) <b>{description}</b> <b>{response.status_code}</b> {millis}ms.')
+        else:
+            return format_status(STATES['WARN'], 'Unable to authenticate.')
+    except Exception as e:
+            print(e)
+            return format_status(STATES['FAIL'], f'fout bij ophalen status code van: {url}')
+    
 def check_status_code(url: str) -> str:
     try:
         response = requests.get(url, allow_redirects=True)
@@ -20,7 +39,7 @@ def check_status_code(url: str) -> str:
             status = STATES['FAIL']
         millis = response.elapsed / timedelta(milliseconds=1)
 
-        return format_status(status, f'[{urlsplit(url).netloc}]({url}) status code: <b>{response.status_code}</b> respons duurde {millis}ms.')
+        return format_status(status, f'[{urlsplit(url).netloc}]({url}) <b>{response.status_code}</b> {millis}ms.')
 
     except Exception as e:
         print(e)
@@ -48,7 +67,6 @@ def check_ldv_service_status(service_uri: str) -> str:
             status = STATES['WARNING']
         elif now_cet - cr <= timedelta(days=1):
              status = STATES['WARNING']
-             cr = f'<u>{cr}</u>'
         else:
             status = STATES['OK']
 
@@ -114,6 +132,12 @@ def main():
         check_ldv_service_status('https://api.linkeddata.cultureelerfgoed.nl/datasets/thesauri/cht/services/cht-jena/'),
         check_ldv_service_status('https://api.linkeddata.cultureelerfgoed.nl/datasets/thesauri/cht/services/cht-virtuoso/'),
         check_ldv_service_status('https://api.linkeddata.cultureelerfgoed.nl/datasets/thesauri/archeologischbasisregister/services/archeologischbasisregister-jena/'),
+        poolparty_test('https://digitaalerfgoed-test.poolparty.biz/PoolParty/api/thesaurus/312d0e18-773b-45eb-95ff-51a94d760967/concept?concept=https://digitaalerfgoed-test.poolparty.biz/term/id/cht/9f841236-c0d0-4583-bfab-b2e8ddcf8fec&properties=all', 'Get concept status', TOKEN),
+        poolparty_test('https://digitaalerfgoed-test.poolparty.biz/PoolParty/api/thesaurus/312d0e18-773b-45eb-95ff-51a94d760967/narrowers?concept=https://digitaalerfgoed-test.poolparty.biz/term/id/cht/04770e4f-1c11-40e7-a256-9de8c6d569c6&properties=all', 'Get narrowers', TOKEN),
+        poolparty_test('https://digitaalerfgoed-test.poolparty.biz/PoolParty/api/thesaurus/312d0e18-773b-45eb-95ff-51a94d760967/broaders?concept=https://digitaalerfgoed-test.poolparty.biz/term/id/cht/9f841236-c0d0-4583-bfab-b2e8ddcf8fec&properties=all&properties=all', 'Get broaders', TOKEN),
+        poolparty_test('https://data.cultureelerfgoed.nl/PoolParty/api/thesaurus/1DF17ED4-4A38-0001-C6FF-883013B04AD0/concept?concept=https://data.cultureelerfgoed.nl/term/id/cht/1b8bd4e8-d51c-4ae2-8c16-c56751e2c470&properties=all&language=de','Zoeken concept', TOKEN),
+        poolparty_test('https://data.cultureelerfgoed.nl/PoolParty/api/thesaurus/1DF17ED4-4A38-0001-C6FF-883013B04AD0/concept?concept=https://data.cultureelerfgoed.nl/term/id/cht/1685e55b-68a3-421f-9bf8-a64b6ec269b7', 'Geef me de het prefLabel van een bepaalde term.', TOKEN),
+        poolparty_test('https://data.cultureelerfgoed.nl/extractor/api/suggest?projectId=1DF17ED4-4A38-0001-C6FF-883013B04AD0&language=nl&searchString=gotiek', 'Staat een bepaalde term in de thesaurus?', TOKEN),
         check_status_code('https://data.cultureelerfgoed.nl/term/id/abr/b402446a-0a00-4fee-a9cd-1a7f307d651e.html'),
         check_status_code('https://kennis.cultureelerfgoed.nl/index.php/Datasets_van_de_RCE'),
         check_status_code('https://beeldbank.cultureelerfgoed.nl/'),
